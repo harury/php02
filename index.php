8107c7045396f4f1dc2f737e3348dfbbe26ef21c
Harury <matheusribeirofg1@gmail.com>
2019-08-08 20:23:56 -0300

    of-dae

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
     <title> Aula PHP </title>
 </head>
 <body>
-    
+    <h2>hello world</h2>
 </body>
 </html>
